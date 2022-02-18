a3bd58e43df9311d224c32e7079d3ca45c097a5a
githubfollowme <flymayday112@gmail.com>
2022-02-18 15:50:26 +0800

    在商品區顯示分類文字導覽列

--- front/main.php
+++ front/main.php
@@ -1,10 +1,20 @@
-
-
-$rows=$Goods->all()
-
-如果是中分類的話 sh=>1,mid=>type
-
 <?php
-foreach rows as row
+$type=$_GET['type']??0;
+if($type==0){
+    $nav="全部商品";
+}else{
+    $t=$Type->find($type);
+    if($t['parent']==0){
+        $nav=$t['name'];
+    }else{
+        $b=$Type->find($t['parent']);
+        $nav= $b['name'] ." > ".$t['name'];
+    }
+}
+
 
 ?>
+
+
+
+<h1><?=$nav;?></h1>
